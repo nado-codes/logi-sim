529d61825bd0f170293b03e83833e7036928adb6
Nathan <nadocouniversal@gmail.com>
2026-04-29 21:16:37 +1000

    Fixed Office Lighting + Presentation, Added AI Employees

---
+++ unity-client/Assets/Scripts/WanderingNPC.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderingNPC : MonoBehaviour
+{
+    public Transform[] waypoints;  // Drag desk + vending machine + water cooler in inspector
+    public float minWait = 5f;
+    public float maxWait = 15f;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(WanderRoutine());
+    }
+
+    IEnumerator WanderRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+            Transform target = waypoints[Random.Range(0, waypoints.Length)];
+            agent.SetDestination(target.position);
+        }
+    }
+}
