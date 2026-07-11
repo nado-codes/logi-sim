67b137a12c432588dfcbe5026de791cc1ff0cfb2
Nathan <67047470+nado-codes@users.noreply.github.com>
2026-07-12 09:45:43 +1000

    working on prompt system (for use with debt notifications)

---
+++ unity-client/Assets/Scripts/UI/Windows/Prompt.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(UIActionController))]
+public class Prompt : BaseWindow<Prompt>
+{
+    private static Prompt Instance;
+    private UIActionController actionController;
+
+    public GameObject promptPrototype;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        actionController = GetComponent<UIActionController>();
+
+        if(Instance == null)
+        {
+            Instance = this;
+            Show("Welcome To LogiSim!", "Your adventure begins~");
+        }
+
+    }
+
+    public static void Show(string title, string message)
+    {
+        if(Instance == null)
+        {
+            Debug.LogError("Prompt instance is null. Make sure there is a Prompt component in the scene");
+            return;
+        }
+        if(Instance.promptPrototype == null)
+        {
+            Debug.LogError("Prompt prototype is null. Make sure the Prompt component has a promptPrototype set");
+            return;
+        }
+
+        var prompt = Instantiate(Instance.promptPrototype);
+        prompt.transform.position = Instance.promptPrototype.transform.position;
+        var texts = prompt.GetComponentsInChildren<TextMeshProUGUI>();
+        var txTitle = texts.FirstOrDefault(t => t.name == "txWindowTitle");
+        var messageText = texts.FirstOrDefault(t => t.name == "txPromptBody");
+        var promptComponent = prompt.GetComponent<Prompt>();
+
+        if(txTitle == null || messageText == null)
+        {
+            Debug.LogError("Prompt prototype must have a TextMeshProUGUI called txWindowTitle and txPromptBody");
+            return;
+        }
+        if(promptComponent == null)
+        {
+            Debug.LogError("Prompt prototype must have a Prompt component");
+            return;
+        }
+
+        txTitle.text = title;
+        messageText.text = message;
+        promptComponent.GetComponent<CanvasGroupToggle>().Show();
+    }
+
+}
